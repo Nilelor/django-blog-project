6fe361d2b49611d269e740b39aa813f22eb4b372
Dede Ankrah <pcspida@ubuntu.(none)>
2012-07-05 21:33:42 +0000

    seth

--- blog/models.py
+++ blog/models.py
@@ -5,15 +5,36 @@ from django.contrib import admin
 class Post(models.Model):
 	title=models.CharField(max_length=60)
 	body=models.TextField()
-	created=models.DateField()
-	updated=models.DateField()
+	created=models.DateField(auto_now_add=True)
+	updated=models.DateField(auto_now=True)
+	def __unicode__(self):
+		return self.title
 
 class Comment(models.Model):
 	body=models.TextField()
 	author=models.CharField(max_length=60)
-	created=models.DateField()
-	updated=models.DateField()
+	created=models.DateField(auto_now_add=True)
+	updated=models.DateField(auto_now=True)
 	post=models.ForeignKey(Post)
+	def exactBody(self):
+		return self.body[:60]
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+
+class CommentInline(admin.TabularInline):
+	model=Comment
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title','created','updated')
+	search_fields = ('title','body')
+	list_filter = ('created',)
+	inlines = [CommentInline]
+
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('author','exactBody','created','updated')
+	list_filter = ('created','author')
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Comment,CommentAdmin)	
+	
+
